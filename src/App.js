c396d19697c6007abf1a2aa80e0584026a38894d
Gabriel Borges <gabriel.borges@happy.com.br>
2024-10-18 17:41:24 -0300

    new array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Front-end',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Data-science',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '',
+      corSecundaria: ''
+    }
+  ]
 
   const [colaboradores, setColaboradores] = useState([])
 
@@ -17,6 +56,13 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome="Programação"/>
+      <Time nome="Front-End"/>
+      <Time nome="Data Science"/>
+      <Time nome="Devops"/>
+      <Time nome="UX e Design"/>
+      <Time nome="Mobile"/>
+      <Time nome="Inovação e Gestão"/>
     </div>
   );
 }
